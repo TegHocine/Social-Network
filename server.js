18783d5075eeb1cb04033d5f58b90a741d83121a
TegHocine <arcou98@hotmail.com>
2022-02-07 14:30:15 +0100

    feat!: api to auth the user and get token + middleware

--- server.js
+++ server.js
@@ -8,10 +8,23 @@ app.use(express.json());
 // connect database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
